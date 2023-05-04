50b527644dbf2a8b4612975bf367882969642c62
강민수 <naman79@naver.com>
2023-05-04 12:51:21 +0900

    add first files

---
+++ src/main/java/com/platyduck/server/controller/FileController.java
@@ -0,0 +1,44 @@
+package com.platyduck.server.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+@RequestMapping("/file")
+public class FileController {
+
+    @GetMapping("/upload")
+    public String file(Model model) {
+
+        System.out.println("FileController");
+
+        return "upload.html";
+    }
+
+    @GetMapping("/fileUpload")
+    public String uploadForm(Model model) {
+
+        System.out.println("FileController");
+
+        return "uploadForm.html";
+    }
+
+    //fileUpload
+    @PostMapping("/fileUpload")
+    public String fileUpload(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) {
+
+
+
+        redirectAttributes.addFlashAttribute("message",
+                "You successfully uploaded " + file.getOriginalFilename() + "!");
+
+        return "redirect:/file/fileUpload";
+    }
+
+}
