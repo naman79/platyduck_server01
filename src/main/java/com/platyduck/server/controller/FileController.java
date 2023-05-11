cf1bacbd107f9b46862d046906b95ba978c63b09
강민수 <naman79@naver.com>
2023-05-11 17:04:56 +0900

    Add ajax file upload
    add ajax file upload

--- src/main/java/com/platyduck/server/controller/FileController.java
+++ src/main/java/com/platyduck/server/controller/FileController.java
@@ -1,18 +1,20 @@
 package com.platyduck.server.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.swing.*;
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -25,14 +27,6 @@ public class FileController {
     private String uploadPath;
 
     @GetMapping("/upload")
-    public String file(Model model) {
-
-        System.out.println("FileController");
-
-        return "upload.html";
-    }
-
-    @GetMapping("/fileUpload")
     public String uploadForm(Model model) {
         File dirFile = new File(uploadPath);
         File[] fileList = dirFile.listFiles();
@@ -64,7 +58,7 @@ public class FileController {
     }
 
     //fileUpload
-    @PostMapping("/fileUpload")
+    @PostMapping("/upload")
     public String fileUpload(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) throws IOException {
 
         System.out.println("FileController");
@@ -72,7 +66,65 @@ public class FileController {
         redirectAttributes.addFlashAttribute("message",
                 "You successfully uploaded " + file.getOriginalFilename() + "!");
 
-        return "redirect:/file/fileUpload";
+        return "redirect:/file/upload";
     }
+
+
+    @GetMapping("/upload-ajax")
+    public String uploadAjax(Model model) {
+        File dirFile = new File(uploadPath);
+        File[] fileList = dirFile.listFiles();
+        List<String> ret = new ArrayList<>();
+
+        if(fileList == null) {
+            return "uploadForm.html";
+        }
+
+        Arrays.sort(fileList);
+        Arrays.stream(fileList).sorted().filter(File::isFile).forEach( file -> {
+            ret.add(file.getName());
+        });
+
+//        for (File file : fileList) {
+//
+//            if(file.isFile()) {
+//
+//            }
+//
+//            if (file.isFile()) {
+//                ret.add(file.getName());
+//            }
+//        }
+
+        model.addAttribute("files", ret);
+
+        return "uploadAjax.html";
+    }
+
+    @RequestMapping(value = "/uploadFile", method = RequestMethod.POST)
+    @ResponseBody
+    public ResponseEntity<?> uploadFile(
+            @RequestParam("uploadfile") MultipartFile uploadfile) {
+
+        try {
+            // Get the filename and build the local file path (be sure that the
+            // application have write permissions on such directory)
+            String filename = uploadfile.getOriginalFilename();
+            String directory = uploadPath;
+            String filepath = Paths.get(directory, filename).toString();
+
+            // Save the file locally
+            BufferedOutputStream stream =
+                    new BufferedOutputStream(new FileOutputStream(new File(filepath)));
+            stream.write(uploadfile.getBytes());
+            stream.close();
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    } // method uploadFile
 
 }
