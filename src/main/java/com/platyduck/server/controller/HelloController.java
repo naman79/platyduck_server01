d7c4f7eda5e8260adc027e6ccfe58d89ef95af98
강민수 <naman79@naver.com>
2023-05-12 17:47:39 +0900

    add post json type
    add post json type controller

--- src/main/java/com/platyduck/server/controller/HelloController.java
+++ src/main/java/com/platyduck/server/controller/HelloController.java
@@ -1,8 +1,16 @@
 package com.platyduck.server.controller;
 
+import com.platyduck.server.domain.Param;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.nio.file.Paths;
 
 @Controller
 @RequestMapping("/")
@@ -15,4 +23,20 @@ public class HelloController {
 
             return "hello.html";
         }
+
+    @RequestMapping(value = "/call-api", method = RequestMethod.POST)
+    @ResponseBody
+    public ResponseEntity<?> callApi(
+            @RequestBody Param param) {
+
+        try {
+            System.out.println("apiTarget: " + param.getData1());
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    } // method uploadFile
 }
---
+++ src/main/java/com/platyduck/server/domain/Param.java
@@ -0,0 +1,15 @@
+package com.platyduck.server.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Param {
+
+    private String data1;
+    private String data2;
+
+}
